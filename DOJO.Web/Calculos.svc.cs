5639a2c4e7f6027766aab4a2a4fee88f54b293f0
Pablo Juan <pablojuan@foryoursystem.com>
2014-10-01 17:00:28 -0300

    Primeiro Commit Resolucao Dojo

---
+++ DOJO.Web/Calculos.svc.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Text;
+
+namespace DOJO.Web
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Calculos
+    {
+        private char[,] teste { get; set; }
+        [OperationContract]
+        public void DoWork()
+        {
+            // Add your operation implementation here
+            return;
+        }
+
+        private static char[,] BiDimensionalMatriz(string resultado)
+        {
+
+            char[,] rollback = new char[10, 10];
+            var rows = resultado.Split(';');
+
+            for (int i = 0; i < 10; i++)
+            {
+                var columns = rows[i].Split('|');
+
+                for (int j = 0; j < 10; j++)
+                {
+                    Char.TryParse(columns[j].ToString(), out rollback[i, j]);
+                }
+            }
+            return rollback;
+
+        }
+
+        private int ContarAeroportos(char[,] Mapa)
+        {
+            int cont = 0;
+
+            for (int i = 0; i < Mapa.GetLength(0); i++)
+            {
+                for (int j = 0; j < Mapa.GetLength(1); j++)
+                {
+                    if (Mapa[i, j] == 'A')
+                    {
+                        cont += 1;
+                    }
+                }
+            }
+
+            return cont;
+        }
+
+
+        private static char[,] Expandir(char[,] Mapa)
+        {
+            char[,] quadrado = new char[10, 10];
+            for (int i = 0; i < Mapa.GetLength(0); i++)
+            {
+                for (int j = 0; j < Mapa.GetLength(1); j++)
+                {
+                    if (Mapa[i, j] == ' ')
+                    {
+                        quadrado[i, j] = ' ';
+                    }
+                    if (Mapa[i, j] == 'A')
+                    {
+                        quadrado[i, j] = 'A';
+                    }
+                    if (Mapa[i, j] == '*')
+                    {
+                        quadrado[i, j] = '*';
+                        if (j > 0)
+                            quadrado[i, j - 1] = '*';
+                        if (j < 9)
+                            quadrado[i, j + 1] = '*';
+                        if (i > 0)
+                            quadrado[i - 1, j] = '*';
+                        if (i < 9)
+                            quadrado[i + 1, j] = '*';
+                    }
+                }
+            }
+
+            return quadrado;
+
+
+        }
+
+        private static string CharToString(char[,] quadrado)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    sb.Append(quadrado[i, j].ToString());
+                    sb.Append("|");
+                }
+                sb.Append(";");
+            }
+            return sb.ToString();
+        }
+
+        [OperationContract]
+        public string PrimeiroDia()
+        {
+            Mapa();
+            return CharToString(teste);
+        }
+
+        private void Mapa()
+        {
+            teste = new char[,]{
+                {' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' '},
+                {' ', '*', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', '*'},
+                {' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', 'A', ' ',' ', ' ', 'A', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', 'A',' ', ' ', ' ', ' ', '*'},
+                {' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', '*', ' ', ' ',' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' '}
+            };
+        }
+
+
+        [OperationContract]
+        public string PrimeiroAeroporto()
+        {
+            Mapa();
+            while (true)
+            {
+                teste = Expandir(teste);
+                int aeroportos = ContarAeroportos(teste);
+                if (aeroportos <= 2)
+                    break;
+            }
+            return CharToString(teste);
+        }
+
+
+        [OperationContract]
+        public string TodosAeroportos()
+        {
+            Mapa();
+            while (true)
+            {
+                teste = Expandir(teste);
+                int aeroportos = ContarAeroportos(teste);
+                if (aeroportos <= 0)
+                    break;
+            }
+            return CharToString(teste);
+        }
+
+        [OperationContract]
+        public int DiasTodosAeroportos()
+        {
+            int count = 0;
+            Mapa();
+            while (true)
+            {
+                count++;
+                teste = Expandir(teste);
+                int aeroportos = ContarAeroportos(teste);
+                if (aeroportos <= 0)
+                    break;
+            }
+
+            return count;
+        }
+
+        [OperationContract]
+        public int DiasPrimeiroAeroporto()
+        {
+            int count = 0;
+            Mapa();
+            while (true)
+            {
+                count++;
+                teste = Expandir(teste);
+                int aeroportos = ContarAeroportos(teste);
+                if (aeroportos <= 2)
+                    break;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ DOJO/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using DOJO.ServicoCalculo;
+
+namespace DOJO
+{
+    public partial class MainPage : UserControl
+    {
+        int _vez = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var cliente = new CalculosClient();
+            cliente.PrimeiroDiaCompleted += new EventHandler<PrimeiroDiaCompletedEventArgs>(PrimeiroDia);
+            cliente.PrimeiroDiaAsync();
+
+
+            cliente.PrimeiroAeroportoCompleted += new EventHandler<PrimeiroAeroportoCompletedEventArgs>(PrimeiroAeroporto);
+            cliente.PrimeiroAeroportoAsync();
+
+
+            cliente.TodosAeroportosCompleted += new EventHandler<TodosAeroportosCompletedEventArgs>(TodosAeroportos);
+            cliente.TodosAeroportosAsync();
+
+            cliente.DiasPrimeiroAeroportoCompleted += new EventHandler<DiasPrimeiroAeroportoCompletedEventArgs>(DiasPrimeiroAeroporto);
+            cliente.DiasPrimeiroAeroportoAsync();
+
+
+            cliente.DiasTodosAeroportosCompleted += new EventHandler<DiasTodosAeroportosCompletedEventArgs>(DiasTodosAeroportos);
+            cliente.DiasTodosAeroportosAsync();
+
+        }
+
+        private void DiasTodosAeroportos(object sender, DiasTodosAeroportosCompletedEventArgs e)
+        {
+            lblTodos.Content = String.Format("Todos os aeroportos estarão cobertos em {0} dias", e.Result);
+        }
+
+        private void DiasPrimeiroAeroporto(object sender, DiasPrimeiroAeroportoCompletedEventArgs e)
+        {
+            lblPrimeiro.Content = String.Format("O primeiro Aeroporto estará coberto em {0} dias", e.Result);
+        }
+
+        private void TodosAeroportos(object sender, TodosAeroportosCompletedEventArgs e)
+        {
+            var Mapa = BiDimensionalMatriz(e.Result);
+            BindDataGrid(Mapa, gridTodos);
+        }
+
+        private void PrimeiroDia(object sender, PrimeiroDiaCompletedEventArgs e)
+        {
+            var Mapa = BiDimensionalMatriz(e.Result);
+            BindDataGrid(Mapa, gridInicio);
+        }
+
+        private void PrimeiroAeroporto(object sender, PrimeiroAeroportoCompletedEventArgs e)
+        {
+            var Mapa = BiDimensionalMatriz(e.Result);
+            BindDataGrid(Mapa, gridPrimeiro);
+        }
+
+
+        char[,] BiDimensionalMatriz(string resultado)
+        {
+
+            char[,] rollback = new char[10, 10];
+            var rows = resultado.Split(';');
+
+            for (int i = 0; i < 10; i++)
+            {
+                var columns = rows[i].Split('|');
+
+                for (int j = 0; j < 10; j++)
+                {
+                    Char.TryParse(columns[j].ToString(), out rollback[i, j]);
+                }
+            }
+            return rollback;
+
+        }
+        void BindDataGrid(char[,] _array, DataGrid grid)
+        {
+            ObservableCollection<ObservableCollection<char>> dataSource = new ObservableCollection<ObservableCollection<char>>();
+            for (int i = 0; i < 10; i++)
+            {
+                ObservableCollection<char> row = new ObservableCollection<char>();
+                for (int j = 0; j < 10; j++)
+                {
+                    row.Add(_array[i, j]);
+                }
+                dataSource.Add(row);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                DataGridTextColumn dataColumn = new DataGridTextColumn();
+                dataColumn.Header = "       ";
+                dataColumn.Binding = new Binding("[" + i.ToString() + "]");
+                grid.Columns.Add(dataColumn);
+            }
+            grid.ItemsSource = dataSource;
+        }
+    }
+}
